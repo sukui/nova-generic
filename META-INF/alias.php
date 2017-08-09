adca6d2e2fa835ee287ef6c8dace427600bd4e0b
chuxiaofeng <chuxiaofeng@youzan.com>
2017-08-09 19:17:14 +0800

    Initial commit

---
+++ META-INF/alias.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    \ZanPHP\NovaGeneric\GenericRequestCodec::class => "\\Zan\\Framework\\Network\\Tcp\\GenericRequestCodec",
+    \ZanPHP\NovaGeneric\GenericInvokeException::class => "\\Zan\\Framework\\Network\\Exception",
+];
---
+++ src/GenericInvokeException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ZanPHP\NovaGeneric;
+
+use ZanPHP\Exception\ZanException;
+
+class GenericInvokeException extends ZanException
+{
+
+}
